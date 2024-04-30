72d2f68ade1082ec8daf5b56f460efe2ffa26582
Cory Calahan <corycalahan@github.com>
2024-04-30 09:07:42 -0700

    Create testCode_xss.py

---
+++ test/testCode_xss.py
@@ -0,0 +1,10 @@
+from flask import Flask, request, make_response
+
+app = Flask(__name__)
+
+
+@app.route('/lgin')
+def lgin():
+
+    first_name = request.args.get('name', '')
+    return make_response("Your name" + first_name )
